c5d342fdf43338c86b258b497cb975b622645383
joshbeyerr <117863098+joshbeyerr@users.noreply.github.com>
2022-11-12 19:10:54 -0500

    Update mainRun.py

--- mainRun.py
+++ mainRun.py
@@ -1,7 +1,8 @@
+import time
+
 import stockxApi
 import website
 
-sku = '53912950'
 fees = 0.12
 
 searchApi = 'https://www.footlocker.ca/api/products/search?query=greatdeals%3Arelevance%3Aproducttype%3AShoes%3Abrand%3AJordan%3Abrand%3ANike&currentPage=&sort=relevance&pageSize=48&timestamp=4'
@@ -12,37 +13,40 @@ def runStockx(sku):
     return shoeInfo
 
 
-def sort(thisSku, footlockerShoe, stockxInfo):
-    price = footlockerShoe[thisSku]['price']
-    sizes = footlockerShoe[thisSku]['sizes']
+def sort(thisSku, footsiteShoe, stockxInfo):
+    price = footsiteShoe[thisSku]['price']
+    sizes = footsiteShoe[thisSku]['sizes']
     name = stockxInfo['Name']
     print('')
     print(name)
     profit = False
 
     for i in sizes:
-        stockxSizes = stockxInfo['sizes'][str(i)]
-        if stockxSizes['estimatedPrice'] != None:
-            estimatedPrice = float(stockxSizes['estimatedPrice'])
-            payout = round((estimatedPrice * (1 - fees)), 2)
-        else:
-            payout = 0
+        try:
+            stockxSizes = stockxInfo['sizes'][str(i)]
+            if stockxSizes['estimatedPrice'] != None:
+                estimatedPrice = float(stockxSizes['estimatedPrice'])
+                payout = round((estimatedPrice * (1 - fees)), 2)
+            else:
+                payout = 0
 
-        if (payout - 10) > float(price):
-            profit = True
-            print('Size {} is profitable with a price of {} and a payout of {}'.format(i, price, payout))
+            if (payout - 10) > float(price):
+                profit = True
+                print('Size {} is profitable with a price of {} and a payout of {}'.format(i, price, payout))
+        except:
+            print('Size {} not on stockx'.format(i))
 
     if profit == False:
-        print('No profitable sizes')
+        print('No good sizes')
         # else:
         #     print('Size {} is NOT profitable with a price of {} and a payout of {}'.format(i, price, payout))
 
 
 def siteChoose():
-    siteList = ['footlocker']
+    siteList = ['stockx', 'footlocker', 'champs', 'adidas']
     print('Sites Available: ', end='')
     for x in siteList:
-        print(x.title(), end='')
+        print('{}, '.format(x.title()), end='')
     print('')
 
     siteSelect = False
@@ -64,19 +68,20 @@ def main():
     runSku = False
     while not runSku:
 
-        if userSite == 'footlocker':
-            skuInput = input('Please enter footlocker sku (x to quit): ')
+        if userSite == 'footlocker' or userSite == 'champs':
+            skuInput = input('Please enter {} sku (x to quit): '.format(userSite))
             if skuInput == 'x':
                 quit()
 
             print(' ')
-            print('Getting info from footlocker... ')
-            footlockerShoe = website.footlocker(skuInput)
+            print('Getting info from {}... '.format(userSite))
+            footsiteShoe = website.footsite(userSite, skuInput)
 
-            print(footlockerShoe)
+            print(footsiteShoe)
 
             # for each sku in footlocker list
-            for thisSku in footlockerShoe:
+            for thisSku in footsiteShoe:
+                time.sleep(2)
                 print('Getting Stockx info for {}...'.format(thisSku))
                 stockxInfo = runStockx(thisSku)
                 print(stockxInfo)
@@ -86,10 +91,60 @@ def main():
 
                 else:
 
-                    sort(thisSku, footlockerShoe, stockxInfo)
+                  sort(thisSku, footsiteShoe, stockxInfo)
+
+        elif userSite == 'stockx':
+            skuInput = input('Please enter sku (x to quit): '.format(userSite))
+            if skuInput == 'x':
+                quit()
+            stockxInfo = runStockx(skuInput)
+
+            name = stockxInfo['Name']
+            print('')
+            print('\n {}'.format(name))
+            stockxSizes = stockxInfo['sizes']
+            for x in stockxSizes:
+                if stockxSizes[x]['estimatedPrice'] != None:
+                    estimatedPrice = float(stockxSizes[x]['estimatedPrice'])
+                    payout = round((estimatedPrice * (1 - fees)), 2)
+                else:
+                    payout = 0
+
+                print('Size {} Payout: {}'.format(x, payout))
+
+        elif userSite == 'adidas':
+
+            a = input('Type go: ').lower()
+            if a == 'go':
+                pass
+            else:
+                quit()
+
+
+            print(' ')
+            print('Getting info from {}... '.format(userSite))
+            adidasShoes = website.adidas()
+
+            print(adidasShoes)
+
+            # for each sku in footlocker list
+            for thisSku in adidasShoes:
+                print('Getting Stockx info for {}...'.format(thisSku))
+                stockxInfo = runStockx(thisSku)
+                print(stockxInfo)
+
+                if stockxInfo == None:
+                    print('No Product')
+
+                else:
+
+                    sort(thisSku, adidasShoes, stockxInfo)
+
 
         else:
+
             print('Not Ready Yet')
 
 
 main()
+
